50725c8b44a420352ea3b8deaa2ef0c5897b697c
David Fuhr <fuhr@flagbit.de>
2011-05-10 20:35:30 +0200

    added correct html doctype

--- src/configcheck.php
+++ src/configcheck.php
@@ -146,7 +146,12 @@ foreach ($settingValidators as $settingValidator) {
 
 
 // start output
-?>
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+    <title>configcheck</title>
+</head>
+<body>
 <dl>
 <?php foreach ($output as $settingOutput) : ?>
 	<dt><?php echo htmlspecialchars($settingOutput['name']) ?></dt>
@@ -158,4 +163,6 @@ foreach ($settingValidators as $settingValidator) {
 	</dd>
 <?php endforeach; ?>
 </dl>
+</body>
+</html>
 
